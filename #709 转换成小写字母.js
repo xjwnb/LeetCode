4694911266f76aeaa265a0ee0c90228f6ae449b6
1565163445@qq.com <1565163445@qq.com>
2020-11-28 14:37:11 +0800

    update 转换成小写字母

---
+++ #709 转换成小写字母.js
@@ -0,0 +1,43 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-28 14:33:18
+ * @LastEditTime: 2020-11-28 14:36:41
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Leetcode\#709 转换成小写字母.js
+ */
+
+/*
+实现函数 ToLowerCase()，该函数接收一个字符串参数 str，
+并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。
+
+示例 1：
+  输入: "Hello"
+  输出: "hello"
+
+示例 2：
+  输入: "here"
+  输出: "here"
+
+示例 3：
+  输入: "LOVELY"
+  输出: "lovely"
+*/
+
+var toLowerCase = function (str) {
+  return str
+    .split("")
+    .map((item) => {
+      return item.toLowerCase();
+    })
+    .join("");
+};
+
+
+// 结果
+/* 
+8 / 8 个通过测试用例
+执行用时: 80 ms
+内存消耗: 37.8 MB
+超过 69%
+ */
