ede0212d84e66e01a9965287d418a5d343a61e3b
1565163445@qq.com <1565163445@qq.com>
2021-09-29 16:00:38 +0800

    update

--- 01.简单/test.js
+++ 01.简单/test.js
@@ -1,37 +1,17 @@
 /*
  * @Author: your name
  * @Date: 2021-09-24 17:27:08
- * @LastEditTime: 2021-09-25 19:18:15
+ * @LastEditTime: 2021-09-26 16:45:29
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \LeetCode\01.简单\test.js
  */
-
-var isValid = function (s) {
-  if (s.length % 2 !== 0) return false;
-  let mapStr = {
-    "(": ")",
-    "[": "]",
-    "{": "}",
-  };
-  let res = [];
-  for (let i = 0; i < s.length; i++) {
-    if (mapStr[s[i]]) {
-      res.push(mapStr[s[i]]);
-    } else {
-      if (s[i] === res[res.length - 1]) {
-        res.pop();
-      } else {
-        return false;
-      }
-    }
+var name = "123";
+(function () {
+  if (typeof name === "undefined") {
+    var name = "456";
+    console.log("good " + name);
+  } else {
+    console.log("Hello " + name);
   }
-  return res.length > 0 ? false : true;
-};
-
-/*
-91 / 91 个通过测试用例
-状态：通过
-执行用时: 68 ms
-内存消耗: 37.4 MB
-*/
+})();
